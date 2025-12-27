5729677927918e9812e99b58b4a759acc800d7c7
Mohamed STITOU <stitouu09@gmail.com>
2025-12-27 11:30:17 +0100

    LAST CHANGES

---
+++ test/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+export default App
